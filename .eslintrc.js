10d1c0d1634f32fa8d3c1ac533fe1ec03fbd8019
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-08-30 18:34:41 -0500

    Format the files using Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,12 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-  },
-  extends: [
-    'airbnb-base',
-    'prettier',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {
-  },
+	env: {
+		browser: true,
+		es2020: true,
+	},
+	extends: ['airbnb-base', 'prettier'],
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	rules: {},
 };
